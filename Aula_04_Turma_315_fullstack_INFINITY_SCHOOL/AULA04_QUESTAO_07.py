32c4b3d45fa7797b2c3466ab145033c48c5e40df
Yago Portela Silva de Vasconcelos <theypsv@gmail.com>
2023-10-27 00:52:25 -0300

    Atualizar o AULA04_QUESTAO_07.py

--- Aula_04_Turma_315_fullstack_INFINITY_SCHOOL/AULA04_QUESTAO_07.py
+++ Aula_04_Turma_315_fullstack_INFINITY_SCHOOL/AULA04_QUESTAO_07.py
@@ -5,7 +5,7 @@ while True:
     n3 = float(input("Digite o terceiro número:"))
     n4 = float(input("Digite o quarto número:"))
     n5 = float(input("Digite o quinto número:"))
-    if n1 != n2 and n3 and n4 and n5:
+    if n1 != n2 and n1 != n3 and n1 != n4 and n1 != n5:
         break
     else:
         print("""
@@ -13,15 +13,15 @@ Digite 5 números diferentes!
               """)
         continue
 while True:
-    if n1 > n2 and n3 and n4 and n5 :
+    if n1 > n2 and n1 > n3 and n1 > n4 and n1 > n5 :
         big = n1
-    if n2 > n1 and n3 and n4 and n5 :
+    if n2 > n1 and n2 > n3 and n2 > n4 and n2 > n5 :
         big = n2
-    if n3 > n1 and n2 and n4 and n5 :
+    if n3 > n1 and n3 > n2 and n3 > n4 and n3 > n5 :
         big = n3
-    if n4 > n1 and n2 and n3 and n5 :
+    if n4 > n1 and n4 > n2 and n4 > n3 and n4 > n5 :
         big = n4
-    if n5 > n1 and n2 and n3 and n4 :
+    if n5 > n1 and n5 > n2 and n5 > n3 and n5 > n4 :
         big = n5
     break
 print(f"O maior número é: {big}")
