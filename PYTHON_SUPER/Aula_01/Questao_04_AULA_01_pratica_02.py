f7680b11071daad4cabeda915f2377113d3df33d
Yago Portela Silva de Vasconcelos <theypsv@gmail.com>
2023-11-04 16:33:49 -0300

    s

--- PYTHON_SUPER/Aula_01/Questao_04_AULA_01_pratica_02.py
+++ PYTHON_SUPER/Aula_01/Questao_04_AULA_01_pratica_02.py
@@ -1,11 +1,11 @@
 print("\nDigite 10 Números:")
-par_impar = ""
+n = []
 
-for i in range(1,11):
-    num = int(input(f"\nDigite o #0{i} número:"))
-    if num % 2 == 0:
-        par_impar = "Par"
-        print(f"\nO Número {num} é {par_impar}.")
-    elif num % 2 == 1:
-        par_impar = "ímpar"
-        print(f"\nO Número {num} é {par_impar}.")
+for i in range(10):
+    num = int(input(f"\nDigite o #0{i+1} número:"))
+    n.append(num)
+for i in range(10):
+    if n[i] % 2 == 0:
+        print(f"\nO Número {n[i]} é Par .")
+    elif n[i] % 2 == 1:
+        print(f"\nO Número {n[i]} é ímpar.")
